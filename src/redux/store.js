f513bad505fe7f047516bfdad6c29ba869251b06
Enis <enis.mema@gmail.com>
2023-04-03 11:54:49 +0200

    Fix bugs

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import bookReducer from './books/books';
 import categoriesReducer from './categories/categories';
 
-const store = configureStore({
-  book: bookReducer,
+const rootReducer = combineReducers({
+  books: bookReducer,
   categories: categoriesReducer,
 });
+const store = configureStore({ reducer: rootReducer });
 
 export default store;
