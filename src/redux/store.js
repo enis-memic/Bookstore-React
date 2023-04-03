0c59368c52da1afa327b05e69528ede714e73861
Enis <enis.mema@gmail.com>
2023-04-04 00:03:36 +0200

    Add reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,9 +3,12 @@ import bookReducer from './books/books';
 import categoriesReducer from './categories/categories';
 
 const rootReducer = combineReducers({
-  books: bookReducer,
+  book: bookReducer,
   categories: categoriesReducer,
 });
-const store = configureStore({ reducer: rootReducer });
+
+const store = configureStore({
+  reducer: rootReducer,
+});
 
 export default store;
